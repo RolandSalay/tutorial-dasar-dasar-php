7483a3741add30f59f79d94e0b449ea32aa1c7b1
Roland Salay <150308613+RolandSalay@users.noreply.github.com>
2023-11-24 13:40:48 +0900

    Add files via upload

---
+++ php-html.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Kombinasi PHP & HTML</title>
+</head>
+<body>
+
+<h1>Kombinasi PHP & HTML</h1>
+
+<?php 
+	echo "Hello PHP!";
+?>
+</body>
+</html>
---
+++ variables.php
@@ -0,0 +1,12 @@
+<?php 
+	// contoh penulisan variables di dalam file PHP
+	$title = "SaCode";
+	$tag_line = "Papua Coding School";
+	$tahun = "22022";
+	// Menampilkan data dari dalam variable 
+	echo $title;
+	echo "<br>"; 
+	echo $tag_line;
+	echo "<br>";
+	echo $tahun;
+?>
